5720aa4e5b39c5e49a14365ca2f1ca7d80537cc7
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-03-09 00:53:19 -0700

    Updates

---
+++ precache-manifest.043b0ec4494132533972919dc7f4d9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde949d48039eab5d5b59b275d97afbb",
+    "url": "/css-colors/index.html"
+  },
+  {
+    "revision": "c7fe8b8ac17e4286530a",
+    "url": "/css-colors/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "3299290ea8a0fec81a80",
+    "url": "/css-colors/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "c7fe8b8ac17e4286530a",
+    "url": "/css-colors/static/js/2.9313e36c.chunk.js"
+  },
+  {
+    "revision": "f6e899d155cf3303129298bf4f8b27e8",
+    "url": "/css-colors/static/js/2.9313e36c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3299290ea8a0fec81a80",
+    "url": "/css-colors/static/js/main.db8f68d2.chunk.js"
+  },
+  {
+    "revision": "659e4f67240a958e1eab",
+    "url": "/css-colors/static/js/runtime-main.90c4db32.js"
+  }
+]);
